df1f501e0b9578d713ad539179614ccb8c0d5d79
Ihago <ihagog12@gmail.com>
2019-12-13 16:10:28 -0300

    Rotas Home Preg

--- resources/views/preg/home.blade.php
+++ resources/views/preg/home.blade.php
@@ -19,7 +19,7 @@
   <div class="row justify-content-center">
       <div class="col-sm-4">
           <div class="card-ppc" >
-            <a href="#">
+          <a href="{{route('preg.ajustes')}}">
             <div class="card-ppc-header" ><h3>DISPONÍVEL PARA<br>AJUSTES</h3></div>
             <div class="card-ppc-body">
               <img class="icone" src="{{asset('images/edit-solid.svg')}}" >
@@ -31,7 +31,7 @@
 
       <div class="col-sm-4">
           <div class="card-ppc" >
-            <a href="#">
+            <a href="{{route('preg.finalizados')}}">
             <div class="card-ppc-header"><h3>DISPONÍVEL PARA<br>DOWNLOAD</h3></div>
             <div class="card-ppc-body">
               <img class="icone" src="{{asset('images/download-solid.svg')}}"  >
@@ -43,12 +43,7 @@
 
   </div>
 
-  <div class="row justify-content-center" >
-    <a id="white" class="btn btn-ppc-processo" href="#">
-      PROCESSOS ABERTOS
-    </a>
-
-  </div>
+  
 
 </div>
 @endsection
