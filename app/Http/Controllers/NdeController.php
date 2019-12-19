a954526b261de18b254860023a94c30a31e4de78
Lucas <lucas.siqueira.araujo@gmail.com>
2019-12-19 11:54:59 -0300

    novo ppc

--- app/Http/Controllers/NdeController.php
+++ app/Http/Controllers/NdeController.php
@@ -1,14 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Storage;
+use Lmts\src\controller\LmtsApi;
 use Illuminate\Http\Request;
 
 class NdeController extends Controller
 {
+    private $api;
+
+    public function __construct()
+    {
+      $this->api = new LmtsApi();
+    }
+
     //
     public function index(){
         return view('nde.home');
-        
     }
+
+    
 }
--- app/Http/Controllers/PpcController.php
+++ app/Http/Controllers/PpcController.php
@@ -4,20 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Lmts\src\controller\LmtsApi;
+use App\Ppc;
 
 class PpcController extends Controller
 {
+  private $api;
+
+  public function __construct()
+  {
+    $this->api = new LmtsApi();
+  }
+
+
+
   public function cadastrar(){
       return view('ppc.cadastrar');
   }
 
   public function criar(Request $request){
-    $validator = Validator::make($request->all(), [
-      'arquivo' => 'file|mimes:pdf',
+    $validatedData = $request->validate([
+      'arquivo' => ['file', 'mimes:pdf'],
+      'ano'     => ['required', 'string'],
     ]);
-
-    if($validator->fails()){
-      return redirect()->back()->withErrors($validator->errors());
-    }
+    $file = $request->arquivo;
+    $path = 'editais/';
+    $nome = $request->nome . ".pdf";
+    Storage::putFileAs($path, $file, $nome);
+    Ppc::create([
+      'arquivo' => $path . $nome,
+      'ano'     => $request->ano,
+      'status'  => 'processando',
+    ]);
   }
+
 }
